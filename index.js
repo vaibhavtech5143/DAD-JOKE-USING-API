34236ee667f691b00fb628ec15801e5e2a82610d
Vaibhav Singh <vaibhavsingh2633@gmail.com>
2022-10-03 23:04:44 +0530

    API_USED-TO-FETCH-JOKES

---
+++ index.js
@@ -0,0 +1,14 @@
+// GET https://icanhazdadjoke.com/
+
+document.getElementById("joke-generator").addEventListener("click",joke);
+async function joke(){
+    let config = {
+        headers: {
+            Accept: "application/json",
+        },
+    };
+
+    let a = await fetch("https://icanhazdadjoke.com/" , config);
+    let b = await a.json();
+    document.getElementById("joke-get").innerHTML = b.joke;
+}
